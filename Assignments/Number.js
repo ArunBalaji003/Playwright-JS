1c5b25a6a8d9789100051bb27b3110b0ead56945
ArunBalaji003 <arunbalajiravichandran03@gmail.com>
2025-06-25 14:39:47 +0530

    Add files via upload
    Playwright javascript week1 assignments

---
+++ Assignments/Number.js
@@ -0,0 +1,10 @@
+let num= -6
+
+if(num>0){ 
+    console.log("Number is positive :"+num)
+}else if(num===0){ 
+console.log("Number is Neutral :"+num)
+}
+else{
+    console.log("Number is Negative :"+num)
+}
---
+++ Assignments/OddEven.js
@@ -0,0 +1,12 @@
+let number=10
+
+for (let i=1;i<=number;i++) {
+    if(i % 2 == 0) {
+        console.log("EVEN Number is:"+i);
+    }
+}
+for(let i=1;i<=number;i++) {
+    if(i % 2 !== 0) {
+        console.log("ODD Number is:"+i);
+    }
+}
